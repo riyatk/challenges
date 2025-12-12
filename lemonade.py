f181fe718e3c5b56b74dd1c23f7865e26441fb26
riyatk <riyatk555@gmail.com>
2025-12-12 20:28:22 +0530

    Add files via upload

---
+++ lemonade.py
@@ -0,0 +1,27 @@
+def lemonadeChange(bills):
+    
+    five = 0
+    ten = 0
+
+    for bill in bills:
+        if bill == 5:
+            five += 1
+
+        elif bill == 10:
+            if five == 0:
+                return False
+            five -= 1
+            ten += 1
+
+        else:   
+            if ten > 0 and five > 0:
+                ten -= 1
+                five -= 1
+            elif five >= 3:
+                five -= 3
+            else:
+                return False
+
+    return True
+
+print(lemonadeChange([5,10,5,5,10]))
